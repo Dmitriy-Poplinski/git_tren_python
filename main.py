a7d8ef9b53f811bd703f6341041d215d4bfbcb6e
dmytro-poplinski <dmytro.poplinski@gmail.com>
2022-08-20 14:50:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from minus import minus
+
+print("Hello, it`s my calcualtor")
+print(f"2 + 2 = 4")
+print(f"2 + 4 = 6")
+print(f"2 + 6 = 8")
+print("Далі сам")
+minus()
---
+++ minus.py
@@ -0,0 +1,4 @@
+def minus():
+    print("2 - 2 = 0")
+    print("4 - 2 = 2")
+    print("6 - 2 = 4")
